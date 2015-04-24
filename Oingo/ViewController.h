0aa94ef4a545e3c279cb0135ca4ce2ae05397164
Matthew Acalin <matthewacalin@Matthews-MacBook-Air.local>
2015-04-24 13:51:41 -0700

    Initial Commit

---
+++ Oingo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Oingo
+//
+//  Created by Matthew Acalin on 4/24/15.
+//  Copyright (c) 2015 Oingo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Oingo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Oingo
+//
+//  Created by Matthew Acalin on 4/24/15.
+//  Copyright (c) 2015 Oingo Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
